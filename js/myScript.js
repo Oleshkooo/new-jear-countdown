3847b1a6c098f402338aeaa9496446fe5cc9168d
Oleshko <olehchoma@gmail.com>
2022-01-03 23:16:35 +0200

    -

--- js/myScript.js
+++ js/myScript.js
@@ -1,24 +1,25 @@
 // Час відліку
-var countDownDate = new Date('Jan 1, 2022 00:00:00').getTime();
+let nextYear = new Date().getFullYear() + 1
+let countDownDate = new Date(`Jan 1, ${nextYear} 00:00:00`).getTime();
 
 // Оновлення таймера щосекунди
-var countDownFunction = setInterval(function () {
+let countDownFunction = setInterval(() => {
+    // номер року
+    let nextYear = new Date().getFullYear() + 1
+    let newJear = document.querySelector('#newJear')
+    newJear.innerHTML = `${nextYear} year in`
+
     // Час на зараз
-    var now = new Date().getTime();
+    let now = new Date().getTime();
 
     // Проміжок часу
-    var gap = countDownDate - now;
+    let gap = countDownDate - now;
 
     // Вирахування часу д, г, хв, с
-    var days = Math.floor(gap / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((gap % (1000 * 60)) / 1000);
-
-    if (days<=0 && hours<=0 && minutes<=0 && seconds<=0) {
-        clearInterval(countDownFunction)
-        days = hours = minutes = seconds = 0
-    }
+    let days = Math.floor(gap / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((gap % (1000 * 60)) / 1000);
 
     function addZero(n) {
         return n < 10 ? '0' + n : n;
